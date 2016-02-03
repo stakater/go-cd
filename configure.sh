06cf43a5c7d34dbeb2c0593e0c087de920ecc49f
Atif Saddique <atif@aurorasolutions.io>
2016-02-03 17:49:09 +0500

    app contianer name changed from stakater-app to sample-app

--- configure.sh
+++ configure.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 #----path of the files to be configured----------------
-templateFiles="gocd-data/conf/cruise-config.xml app-compose.yml"
+templateFiles="gocd-data/conf/cruise-config.xml deployment/app-compose.yml"
 
 #---install rpl package---
 sudo apt-get install -y rpl
-
-#------remove the changes in template files if any--------------------
-git checkout $templateFiles
 
 #------configure the specified files using configurations from config.properties file-------
 while read line           
 do           
     IFS='=' read -ra values <<< "$line"
+    echo $line
     prop=${values[0]}
     value=${values[1]}
     rpl -i -e "$prop" "$value" $templateFiles
